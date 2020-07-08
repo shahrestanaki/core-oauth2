76adcb7c47d02861e2295bf465a233770c42efc3
m.shahrestanaki <m.shahrestanaki@hafiz-co.com>
2020-07-08 10:13:24 +0330

    some works

--- src/main/java/com/OAuth2ServerJwtApplication.java
+++ src/main/java/com/OAuth2ServerJwtApplication.java
@@ -1,23 +1,18 @@
 package com;
 
+import com.tools.GetResourceBundle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 
 public class OAuth2ServerJwtApplication {
-  /*  @PostConstruct
-   public void init(){
-        //TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tehran"));   // It will set UTC timezone
-       //// LocalDateTime local = LocalDateTime.now();
-       // local.atZone(ZoneId.systemDefault());
-       // TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.systemDefault()));
-        System.out.println("Spring boot application running in UTC timezone :"+new Date());   // It will print UTC timezone
-
-    }*/
-
     public static void main(String... args) {
         SpringApplication.run(OAuth2ServerJwtApplication.class, args);
+        System.out.println("-------------------- Application start in " + new Date() +
+                " - on port: " + GetResourceBundle.getApplication.getString("server.port") + " ----------------------");
     }
 
 }
